e71177722d935d1616bc9b4322d281ce6ef8d8df
LucaSerafini <lucaserafini31@gmail.com>
2024-03-28 17:25:11 +0100

    fix type

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'denkovi' {
   export interface DenkoviOptions {
     ip: string;
-    model: 'datnetip' | 'smartden';
+    model: 'datnetip' | 'smartdenIO' | 'smartden';
     password?: string;
     user?: string;
     port?: number;
@@ -11,6 +11,6 @@ declare module 'denkovi' {
     constructor(options: DenkoviOptions);
     getStates(): Promise<any>;
     getState(out: number): Promise<any>;
-    setOut(out: number, value: number, status?: boolean): Promise<any>;
+    setOut(out: number, value: 1 | 0, statusPrint?: boolean): Promise<any>;
   }
 }
